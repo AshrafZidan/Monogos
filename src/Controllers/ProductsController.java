1dacbd6b79d1a6604447ea55d0dc6b45bb40d064
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-05-06 10:37:12 +0200

    init

--- src/Controllers/ProductsController.java
+++ src/Controllers/ProductsController.java
@@ -9,15 +9,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import loadFxml.MainUpdateProduct;
 
 /**
  * FXML Controller class
@@ -30,11 +36,50 @@ public class ProductsController implements Initializable {
     @FXML
     private JFXButton addNew;
 
+
+    @FXML
+    private TreeTableView<?> tableProduct;
+
+    @FXML
+    private TreeTableColumn<?, ?> proName;
+
+    @FXML
+    private TreeTableColumn<?, ?> proModel;
+
+    @FXML
+    private TreeTableColumn<?, ?> proAmount;
+
+    @FXML
+    private TreeTableColumn<?, ?> proBuyPrice;
+
+    @FXML
+    private TreeTableColumn<?, ?> proProfit;
+
+    @FXML
+    private TreeTableColumn<?, ?> proSellPrice;
+
+    @FXML
+    private TreeTableColumn<?, ?> proArriveDate;
+
+    @FXML
+    private TreeTableColumn<?, ?> proSuppName;
+
+
+    @FXML
+    void updateProductAction(ActionEvent event) {
+
+//        get selection First
+
+
+        MainUpdateProduct mainUpdateProduct = new MainUpdateProduct("");
+    }
+
     @FXML
     void addNewAction(ActionEvent event) {
 
         try {
 
+            System.out.println("Action Button ");
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/createNewProduct.fxml"));
             Scene scene = new Scene(root);
 
@@ -63,6 +108,145 @@ public class ProductsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+
+
+        // init table
+
     }
+
+    class productTable extends RecursiveTreeObject<productTable> {
+        SimpleStringProperty id;
+        SimpleStringProperty name;
+        SimpleStringProperty model;
+        SimpleStringProperty date;
+        SimpleDoubleProperty amount;
+        SimpleStringProperty suppName;
+        SimpleDoubleProperty buyPrice;
+        SimpleDoubleProperty profit;
+        SimpleDoubleProperty sellPrice;
+
+        public productTable(String id, String name, String model, String date, Double amount, Double buyPrice, Double profit, Double sellPrice, String suppName) {
+            this.id = new SimpleStringProperty(id);
+            this.name = new SimpleStringProperty(name);
+            this.model = new SimpleStringProperty(model);
+            this.date = new SimpleStringProperty(date);
+            this.suppName = new SimpleStringProperty(suppName);
+
+            this.amount = new SimpleDoubleProperty(amount);
+            this.buyPrice = new SimpleDoubleProperty(buyPrice);
+            this.profit = new SimpleDoubleProperty(profit);
+            this.sellPrice = new SimpleDoubleProperty(sellPrice);
+
+        }
+
+        public String getId() {
+            return id.get();
+        }
+
+        public SimpleStringProperty idProperty() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id.set(id);
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public SimpleStringProperty nameProperty() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name.set(name);
+        }
+
+        public String getModel() {
+            return model.get();
+        }
+
+        public SimpleStringProperty modelProperty() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model.set(model);
+        }
+
+        public String getDate() {
+            return date.get();
+        }
+
+        public SimpleStringProperty dateProperty() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date.set(date);
+        }
+
+        public double getAmount() {
+            return amount.get();
+        }
+
+        public SimpleDoubleProperty amountProperty() {
+            return amount;
+        }
+
+        public void setAmount(double amount) {
+            this.amount.set(amount);
+        }
+
+        public String getSuppName() {
+            return suppName.get();
+        }
+
+        public SimpleStringProperty suppNameProperty() {
+            return suppName;
+        }
+
+        public void setSuppName(String suppName) {
+            this.suppName.set(suppName);
+        }
+
+        public double getBuyPrice() {
+            return buyPrice.get();
+        }
+
+        public SimpleDoubleProperty buyPriceProperty() {
+            return buyPrice;
+        }
+
+        public void setBuyPrice(double buyPrice) {
+            this.buyPrice.set(buyPrice);
+        }
+
+        public double getProfit() {
+            return profit.get();
+        }
+
+        public SimpleDoubleProperty profitProperty() {
+            return profit;
+        }
+
+        public void setProfit(double profit) {
+            this.profit.set(profit);
+        }
+
+        public double getSellPrice() {
+            return sellPrice.get();
+        }
+
+        public SimpleDoubleProperty sellPriceProperty() {
+            return sellPrice;
+        }
+
+        public void setSellPrice(double sellPrice) {
+            this.sellPrice.set(sellPrice);
+        }
+    }
+
 
 }
---
+++ src/Controllers/updateProductController.java
@@ -0,0 +1,171 @@
+package Controllers;
+
+import Model.productTransaction;
+import Model.supplierTransaction;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import dialogs.dialog;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ahmed mar3y on 06/05/2018.
+ */
+public class updateProductController implements Initializable {
+
+    @FXML
+    private JFXTextField name;
+
+    @FXML
+    private JFXTextField model;
+
+    @FXML
+    private JFXTextField amount;
+
+    @FXML
+    private JFXTextField buyPrice;
+
+    @FXML
+    private JFXTextField profit;
+
+    @FXML
+    private JFXTextField sellPrice;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private ComboBox supplierName;
+
+    @FXML
+    private JFXButton add;
+    List<String> supplierNames;
+    List<String> supplierIds;
+    List<DBObject> suppliers;
+
+    static String productId;
+
+    @FXML
+    void addAction(ActionEvent event) {
+
+        String pName = name.getText();
+        String pModel = model.getText();
+        String pAmount = amount.getText();
+        String pBuyPrice = buyPrice.getText();
+        String pSellPrice = sellPrice.getText();
+        String pProfit = profit.getText();
+        LocalDate localDate = date.getValue();
+        boolean emptySUpplier = supplierName.getSelectionModel().isEmpty();
+
+        if (
+                pName.trim().isEmpty() ||
+                        pModel.trim().isEmpty() ||
+                        pAmount.trim().isEmpty() ||
+                        pBuyPrice.trim().isEmpty() ||
+                        pSellPrice.trim().isEmpty() ||
+                        pProfit.trim().isEmpty() ||
+                        localDate == null ||
+                        emptySUpplier
+
+                ) {
+
+            dialog Warning = new dialog(Alert.AlertType.WARNING, "خظأ", "ادخل جميع البيانات");
+
+        } else {
+
+
+            int suppIndex = supplierName.getSelectionModel().getSelectedIndex();
+
+            String supplierId = supplierIds.get(suppIndex);
+
+
+            BasicDBObject basicDBObject = new BasicDBObject();
+            basicDBObject.put("name", pName);
+            basicDBObject.put("model", pModel);
+            basicDBObject.put("amount", pAmount);
+            basicDBObject.put("buyPrice", pBuyPrice);
+            basicDBObject.put("profit", pProfit);
+            basicDBObject.put("sellPrice", pSellPrice);
+            basicDBObject.put("date", Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            basicDBObject.put("supplierId", supplierId);
+
+            BasicDBObject basicDBObject1 = productTransaction.insertSupplier(basicDBObject);
+            if (basicDBObject1 != null) {
+
+                resetFields();
+                dialog dd = new dialog(Alert.AlertType.CONFIRMATION, "Done ! ", "Done create Product");
+
+
+            }
+
+
+        }
+
+
+    }
+
+    private void resetFields() {
+
+        this.supplierName.setValue("");
+        this.model.setText("");
+        this.amount.setText("");
+        this.buyPrice.setText("");
+        this.sellPrice.setText("");
+        this.profit.setText("");
+
+        // set init date
+        String newstring = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        date.setValue(LOCAL_DATE(newstring));
+
+    }
+
+    public static final LocalDate LOCAL_DATE(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate localDate = LocalDate.parse(dateString, formatter);
+        return localDate;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+// select all supplier
+        suppliers = supplierTransaction.SelectAllSuppliers();
+        supplierNames = suppliers.stream().map(dbObject -> dbObject.get("name").toString()).collect(Collectors.toList());
+        supplierIds = suppliers.stream().map(dbObject -> dbObject.get("_id").toString()).collect(Collectors.toList());
+        supplierName.getItems().setAll(supplierNames);
+
+
+        // set init date
+        String newstring = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        date.setValue(LOCAL_DATE(newstring));
+
+        // select By productId
+        DBObject dbObject = productTransaction.SelectSupplierById(productId);
+
+//        name.setText(dbObject.get("").toString());
+//        model.setText(dbObject.get("").toString());
+//        amount.setText(dbObject.get("").toString());
+//        buyPrice.setText(dbObject.get("").toString());
+//        profit.setText(dbObject.get("").toString());
+//        sellPrice.setText(dbObject.get("").toString());
+//        date.setValue(null);
+//        supplierName.setValue(dbObject.get("").toString());
+
+
+    }
+}
